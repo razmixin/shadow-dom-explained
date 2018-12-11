948d90615ebf87a1b15f143671667bc61b74d6d3
razmixin <raz594577>
2018-12-11 16:34:20 +0200

    step2

--- shadow-DOM.js
+++ shadow-DOM.js
@@ -1,1 +1,23 @@
 'use strict';
+
+class ShadowsInDom extends HTMLElement {
+  constructor() {
+    super();
+  }
+}
+
+const shadowHost = document.getElementsByClassName('shadow-host')[0];
+const wrapper = document.createElement('div');
+wrapper.setAttribute('class', 'shadows-wrapper');
+
+const ghostOneImg = document.createElement('img');
+const ghostTwoImg = document.createElement('img');
+const ghostThreeImg = document.createElement('img');
+
+ghostOneImg.src = './assets/zwei.jpg';
+ghostTwoImg.src = './assets/booo.png';
+ghostThreeImg.src = './assets/derpy.png';
+
+wrapper.appendChild(ghostOneImg);
+wrapper.appendChild(ghostTwoImg);
+wrapper.appendChild(ghostThreeImg);
